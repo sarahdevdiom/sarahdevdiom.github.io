1c70e3ff0efdbd57a6d131359d7db41e5aaf928c
lchognon7 <73225345+lchognon7@users.noreply.github.com>
2020-10-22 13:47:55 +0200

    Create formulaire.php

---
+++ formulaire.php
@@ -0,0 +1,16 @@
+<html>
+  <head>
+    <meta charset="utf-8"/>
+    <title>Page mot de passe</title>
+  </head>
+  <body>
+    <p>Veuillez entrer le mot de passe</p>
+    <form action="accueil.php" method="post">
+    <p>
+    <input type= "password" name="NotreMotDePasse"
+    <input type= "submit" value="Valider"/>
+    </p>
+    </form>
+  </body>
+</html>
+   
